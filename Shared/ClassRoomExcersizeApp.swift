58f488aa273ec8c3194c691de8a884812c0a7f66
Anastasia Zimina <anastasba@gmail.com>
2021-04-24 12:00:16 -0700

    Initial Commit

---
+++ Shared/ClassRoomExcersizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassRoomExcersizeApp.swift
+//  Shared
+//
+//  Created by Anastasia Zimina on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ClassRoomExcersizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
